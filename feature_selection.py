d420addda938b4976e84149e5c40589aca9c5d7c
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2022-12-08 16:11:25 -0800

    Add nearest neighbor algorithm and documentation

--- feature_selection.py
+++ feature_selection.py
@@ -30,7 +30,33 @@ def feature_selection(data, algorithm):
 
 def k_fold_validation(data, feature_set):
     num_correct = 0
+    for i in range(0, len(data)):
 
+        ## get class label to compare accuracy
+        class_label = data[i][0]
+        ## initialize predicted variable
+        predicted_label = -1
+
+        ## infinite nearest neighbor distance distance and index so the first distance is recorded
+        nn_distance = math.inf
+
+        ## nearest neighbor index variable for debugging purposes
+        # nn = math.inf
+        for j in range(0, len(data)):
+
+            if i != j:  ## do not check yourself otherwise that increases accuracy
+                distance = 0  ## create a sum variable before square root for euclidean distance
+                for k in feature_set:  ## for each feature, find the square of the difference, sum them all, then square root
+                    distance += (data[i][k] - data[j][k]) * (data[i][k] - data[j][k])
+                distance = math.sqrt(distance)
+
+                if distance < nn_distance:  ## if distance is less than previously recorded distance, update nearest neighbor distance and predicted label based on nearest distance
+                    nn_distance = distance
+                    # nn = j for debugging purposes
+                    predicted_label = data[j][0]
+        # print("Object %s is in class %s, NN is %s in class %s" % (i, class_label, nn, predicted_label))
+        if class_label == predicted_label:  ## if prediction matches expectation, record as correct, then return ratio of correct vs attempted.
+            num_correct += 1
     return num_correct / (len(data))
 
 
